bbc3afb8fb93b878e6c85f0229c6d25f76770db8
gleisner <ahashle2@illinois.edu>
2015-12-19 22:56:03 -0500

    basic encoding (no stepping)

---
+++ enigma.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Enigma struct {
+	name      string
+	plugboard map[string]string
+	rotors    [3]Rotor //TODO: change this to a slice?
+	reflector map[string]string
+}
+
+func (e *Enigma) code(msg string) string {
+	var result string
+	for _, r := range msg {
+		c := string(r)
+		fmt.Println("Initial character:" + c)
+		// plugboard mapping, if one exists
+		if p := e.plugboard[c]; p != "" {
+			c = p
+		}
+		// forward journey
+		for _, rotor := range e.rotors {
+			c = rotor.value(c, false)
+			fmt.Println("After rotor " + rotor.name + ": " + c)
+		}
+		e.rotors[0].step += 1
+		// reflector
+		c = e.reflector[c]
+		fmt.Println("After reflector: " + c)
+		for i := len(e.rotors) - 1; i >= 0; i-- {
+			c = e.rotors[i].value(c, true)
+			fmt.Println("After rotor " + e.rotors[i].name + ": " + c)
+		}
+		result += c
+
+	}
+	return result
+}
+
+type Rotor struct {
+	name    string
+	fwiring map[string]string
+	rwiring map[string]string
+	ring    string // the actual rotor
+	roffset int    // ringstellung
+	step    int
+	notch   string
+	// missing: notch
+}
+
+func (r *Rotor) value(c string, reflected bool) string {
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	// adjust for step value for right-hand (entrance) contacts
+	c = string(alphabet[(int(c[0]-'A')+r.step)%26])
+	// if the signal is headed back from the reflector, then we need to use the reverse mapping
+	if reflected {
+		c = r.rwiring[c]
+	} else {
+		c = r.fwiring[c]
+	}
+	// adjust for step value for left-hand (exit) contacts
+	c = string(alphabet[(int(c[0]-'A')-r.step)%26])
+	return c
+}
+func revMap(m map[string]string) map[string]string {
+	mRev := make(map[string]string)
+	for k, v := range m {
+		mRev[v] = k
+	}
+	return mRev
+}
+func main() {
+	var q Enigma
+	q.name = "M3"
+	q.plugboard = map[string]string{}
+	var r1, r2, r3 Rotor
+	r1.name = "I"
+	r1.fwiring = map[string]string{
+		"A": "E",
+		"B": "K",
+		"C": "M",
+		"D": "F",
+		"E": "L",
+		"F": "G",
+		"G": "D",
+		"H": "Q",
+		"I": "V",
+		"J": "Z",
+		"K": "N",
+		"L": "T",
+		"M": "O",
+		"N": "W",
+		"O": "Y",
+		"P": "H",
+		"Q": "X",
+		"R": "U",
+		"S": "S",
+		"T": "P",
+		"U": "A",
+		"V": "I",
+		"W": "B",
+		"X": "R",
+		"Y": "C",
+		"Z": "J",
+	}
+	r1.rwiring = revMap(r1.fwiring)
+	r2.name = "II"
+	r2.fwiring = map[string]string{
+		"A": "A",
+		"B": "J",
+		"C": "D",
+		"D": "K",
+		"E": "S",
+		"F": "I",
+		"G": "R",
+		"H": "U",
+		"I": "X",
+		"J": "B",
+		"K": "L",
+		"L": "H",
+		"M": "W",
+		"N": "T",
+		"O": "M",
+		"P": "C",
+		"Q": "Q",
+		"R": "G",
+		"S": "Z",
+		"T": "N",
+		"U": "P",
+		"V": "Y",
+		"W": "F",
+		"X": "V",
+		"Y": "O",
+		"Z": "E",
+	}
+	r2.rwiring = revMap(r2.fwiring)
+	r3.name = "III"
+	r3.fwiring = map[string]string{
+		"A": "B",
+		"B": "D",
+		"C": "F",
+		"D": "H",
+		"E": "J",
+		"F": "L",
+		"G": "C",
+		"H": "P",
+		"I": "R",
+		"J": "T",
+		"K": "X",
+		"L": "V",
+		"M": "Z",
+		"N": "N",
+		"O": "Y",
+		"P": "E",
+		"Q": "I",
+		"R": "W",
+		"S": "G",
+		"T": "A",
+		"U": "K",
+		"V": "M",
+		"W": "U",
+		"X": "S",
+		"Y": "Q",
+		"Z": "O",
+	}
+	r3.rwiring = revMap(r3.fwiring)
+	q.rotors[0] = r1
+	q.rotors[1] = r2
+	q.rotors[2] = r3
+	q.reflector = map[string]string{
+		"A": "Y",
+		"B": "R",
+		"C": "U",
+		"D": "H",
+		"E": "Q",
+		"F": "S",
+		"G": "L",
+		"H": "D",
+		"I": "P",
+		"J": "X",
+		"K": "N",
+		"L": "G",
+		"M": "O",
+		"N": "K",
+		"O": "M",
+		"P": "I",
+		"Q": "E",
+		"R": "B",
+		"S": "F",
+		"T": "Z",
+		"U": "C",
+		"V": "W",
+		"W": "V",
+		"X": "J",
+		"Y": "A",
+		"Z": "T",
+	}
+	q.code("A")
+}
